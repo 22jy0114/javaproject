178d70be31eafa93728b35c64b9be5d9cb7590a9
22JY0114 <22JY0114@PC06.jynet2.jec.ac.jp>
2023-10-18 11:48:17 +0900

    情報システム開発科を表示

--- javaproject/src/javaproject/HelloWorld.java
+++ javaproject/src/javaproject/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("日本電子専門学校");
+		System.out.println("日本電子専門学校　情報システム開発科");
 	}
 
 }
